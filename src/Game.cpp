1c8a8a785878889b212d2a1a3a586470d711d606
Yahel <yahel.ben.ishay@gmail.com>
2017-11-28 20:46:08 +0200

    added google tests (not working yet) added score count

--- src/Game.cpp
+++ src/Game.cpp
@@ -7,9 +7,12 @@
 Game::Game() {
 	this->playerW = new HumanPlayer(1);
 	this->playerB = new HumanPlayer(-1);
-	this->myboard = new Console();
+	this->myboard = new Console(4,4);
 	this->gameRules = new Reversi_I(myboard);
 	this->myboard->displayBoard();
+	this->score = new int[2];
+	this->score[0] = 2; //White (O) player score
+	this->score[1] = 2; //Black (X) player score
 
 }
 
@@ -24,33 +27,37 @@ void Game::playGame() {
 	int flag = 0;
 	while(true){
 		if(gameRules->isBoardFull()){
-			cout<<"Board Full. Game Over!";
+			cout<<"Board Full. Game Over!"<<endl;
 			break;
 		}
 		else{
-			cout<<"White player O - ";
+			cout<<"White player (O) Choose location: Row Column"<<endl;
 			if(this->gameRules->canPlay(this->playerW)){
-				this->gameRules->play(this->playerW->move());
+				this->gameRules->play(this->playerW->move(),this->score);
+				cout<<endl<<"Score - "<<"White (O): "<<this->score[0]<<", Black (X): "<<this->score[1]<<endl<<endl;
+
 				this->myboard->displayBoard();
 				flag=0;
 			}
-			else{
-				cout<<"White has No moves!"<<endl;
-				if(flag==1){
-					cout<<"Game Over!";
+			else {
+				cout << "White has No moves!" << endl;
+				if (flag == 1) {
+					cout << "Game Over!" << endl;
 					break;
 				}
 				flag = 1;
 			}
 		}
+
 		if(gameRules->isBoardFull()){
-			cout<<"Board Full. Game Over";
+			cout<<"Board Full. Game Over!"<<endl;
 			break;
 		}
 		else{
-			cout<<"Black player X - ";
+			cout<<"Black player (X) Choose location: Row Column"<<endl;
 			if(this->gameRules->canPlay(this->playerB)){
-				this->gameRules->play(this->playerB->move());
+				this->gameRules->play(this->playerB->move(), this->score);
+				cout<<endl<<"Score - "<<"White (O): "<<this->score[0]<<", Black (X): "<<this->score[1]<<endl<<endl;
 				this->myboard->displayBoard();
 				flag = 0;
 			    }
@@ -58,7 +65,7 @@ void Game::playGame() {
 
 				cout<<"Black has No moves!"<<endl;
 				if(flag==1){
-					cout<<"Game Over!";
+					cout<<"Game Over!"<<endl;
 					break;
 				}
 				flag=1;
