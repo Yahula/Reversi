a4f9cc8215a95ab5a32e229929f39afac17f975f
Yahel <yahel.ben.ishay@gmail.com>
2017-11-28 23:05:37 +0200

    AI Player

--- src/AI_Player.cpp
+++ src/AI_Player.cpp
@@ -3,11 +3,16 @@
 //
 
 #include "../include/AI_Player.h"
+#include "../include/GameRules.h"
 #include <iostream>
 using namespace std;
 
-AI_Player::AI_Player(int pNum){
+AI_Player::AI_Player(int pNum, GameRules* g, Board* b){
     this->pNum = pNum;
+    this->gameRules = g;
+    this->board = b;
+
+
 }
 
 AI_Player::~AI_Player() {
