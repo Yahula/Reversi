e8d771d1aec1f882117f7e55aa88a5170ef37a99
Yahel <yahel.ben.ishay@gmail.com>
2017-12-25 14:59:31 +0200

    missing files

---
+++ test/gtestMain.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Yahel Ben ishay on 11/28/17.
+//
+
+#include "../gtest_src/gtest/gtest.h"
+
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
