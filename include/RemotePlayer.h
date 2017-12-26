0c5ec1dfa713940a595a1d9d68ddfc545fae1039
Sagi <sagishaashua@Gmail.com>
2017-12-26 22:23:41 +0200

    comments

--- include/RemotePlayer.h
+++ include/RemotePlayer.h
@@ -12,14 +12,30 @@
 
 
 class RemotePlayer: public Player {
+
 public:
+    /**
+     * contructor.
+     * @param c the client through which the player connects to the server.
+     * @param num
+     */
     RemotePlayer(Client* c, int num);
 
+    /**
+     * ditructor.
+     */
     virtual ~RemotePlayer();
 
+    /**
+     * gets the move the remote player made from the server.
+     * @return a disk the player placed.
+     */
     Disk move();
 
 private:
+    /**
+     * the client that connects to the server.
+     */
     Client* client;
 };
 
--- src/Client.cpp
+++ src/Client.cpp
@@ -132,12 +132,7 @@ void Client::writeStringToServer(char* str) {
         return;
     }
 }
-//
-//char* Client::readStringFromServer(){
-//    char msg[10] = {'\0'};
-//    int r = read(clientSocket,msg, sizeof(msg));
-//    return msg;
-//}
+
 
 
 
